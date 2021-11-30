4ca58b50b1646d5b75a59fd144d6ea6cb572dd1a
Jean Bernardo <jean.bernardo@hp.com>
2021-11-30 13:16:07 -0300

    wip

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -14,6 +14,22 @@ RSpec.describe "routes for Articles", :type => :routing do
             :controller => "articles",
             :action => "show",
             :id => "test",
-          )
+        )
+    end
+end
+
+RSpec.describe "routes for SIGN_UP / SIGN_IN", :type => :routing do
+    it "routes /sign_up to the correct controller and action" do
+        expect(get("/sign_up")).to route_to("registrations#new")
+    end
+    it "routes /sign_up to the correct controller and action" do
+        expect(get("/sign_up")).to_not route_to("registrations#create")
+    end
+    
+    it "routes /log_in to the correct controller and action" do
+        expect(get("/log_in")).to route_to("sessions#new")
+    end
+    it "routes /log_in to the correct controller and action" do
+        expect(get("/log_in")).to_not route_to("sessions#create")
     end
 end
